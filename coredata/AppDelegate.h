3d0cc4b5edbe490c2ebf1682d375a11ceb9f5036
Petr Sima <petr.sima@ackee.cz>
2014-12-02 09:24:59 +0100

    Initial Commit

---
+++ coredata/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  coredata
+//
+//  Created by Petr Šíma on Dec/2/14.
+//  Copyright (c) 2014 Petr Sima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ coredata/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  coredata
+//
+//  Created by Petr Šíma on Dec/2/14.
+//  Copyright (c) 2014 Petr Sima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
